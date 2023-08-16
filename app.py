625c6956cad54ae04bd9719810baade4fa8579cf
Dogukan88 <62509005+Dogukan88@users.noreply.github.com>
2023-08-16 07:27:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,28 +12,28 @@ data2 = []
 data2 = data.copy()
 data2 = pd.DataFrame()
 
-hist_1 = px.histogram(data,x="price")
+hist_1 = px.histogram(data2,x="price")
 st.plotly_chart(hist_1, use_container_width=True)
 
-hist_2 = px.histogram(data,x="year")
+hist_2 = px.histogram(data2,x="year")
 st.plotly_chart(hist_2, use_container_width=True)
 
-hist_3 = px.histogram(data,x="mileage")
+hist_3 = px.histogram(data2,x="mileage")
 st.plotly_chart(hist_3, use_container_width=True)
 
-hist_4 = px.histogram(data,x="lot")
+hist_4 = px.histogram(data2,x="lot")
 st.plotly_chart(hist_4, use_container_width=True)
 
 
-sct_1 = px.scatter(data, x="year", y="lot")
+sct_1 = px.scatter(data2, x="year", y="lot")
 st.plotly_chart(sct_1, use_container_width=True)
 
 
-sct_2 = px.scatter(data, x="year", y="price")
+sct_2 = px.scatter(data2, x="year", y="price")
 st.plotly_chart(sct_2, use_container_width=True)
 
 
-sct_3 = px.scatter(data, x="year", y="mileage")
+sct_3 = px.scatter(data2, x="year", y="mileage")
 st.plotly_chart(sct_3, use_container_width=True)
 
 
