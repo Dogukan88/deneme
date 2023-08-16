2eb299a3b705f2d0b49eb5a68fa590bbb0d9aa71
Dogukan88 <62509005+Dogukan88@users.noreply.github.com>
2023-08-16 06:53:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 
 path = os.path.dirname(__file__)
 data = path+'/USA_cars_datasets.csv'
-df = DataFrame(eval(data))
+df = pd.DataFrame(eval(data))
 
 hist_1 = px.histogram(data,x="price")
 st.plotly_chart(hist_1, use_container_width=True)
