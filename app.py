10320f1f0046cd329b134e91ffe7fa26497cde7c
Dogukan88 <62509005+Dogukan88@users.noreply.github.com>
2023-08-16 06:11:27 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+import os
+
+pwd = os.getcwd()
+data = pd.read_csv(pwd + ("\\notebooks\\USA_cars_datasets.csv"))
+print(data)
+
+
+hist_1 = px.histogram(data,x="price")
+st.plotly_chart(hist_1, use_container_width=True)
+
+hist_2 = px.histogram(data,x="year")
+st.plotly_chart(hist_2, use_container_width=True)
+
+hist_3 = px.histogram(data,x="mileage")
+st.plotly_chart(hist_3, use_container_width=True)
+
+hist_4 = px.histogram(data,x="lot")
+st.plotly_chart(hist_4, use_container_width=True)
+
+
+sct_1 = px.scatter(data, x="year", y="lot")
+st.plotly_chart(sct_1, use_container_width=True)
+
+
+sct_2 = px.scatter(data, x="year", y="price")
+st.plotly_chart(sct_2, use_container_width=True)
+
+
+sct_3 = px.scatter(data, x="year", y="mileage")
+st.plotly_chart(sct_3, use_container_width=True)
+
+
+agree = st.checkbox('Show me year and mileage scatter plot')
+
+if agree:
+    st.plotly_chart(sct_3, use_container_width=True)
